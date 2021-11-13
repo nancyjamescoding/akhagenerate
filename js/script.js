e44e38f148c6f2c9f72f9d5d498bf8876ab05325
nancy nyokabi <nancynyokabi1995@gmail.com>
2021-11-13 21:56:32 +0300

    Added a style.css file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwake']
+let femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+function getName(){
+    let birthday = document.getElementById('birthday').value;
+    let gender = document.getElementById('gender').value;
+    let dayNumber = new Date(birthday).getDay();
+    let result = ''
+
+    if (gender == 'Male'){
+        result =`You were born on ${ daysOfWeek[dayNumber]} 
+        and your akhan name is ${maleNames[dayNumber]}`
+    
+    } 
+    if (gender == 'Female'){
+        result = `You were born on ${ daysOfWeek[dayNumber]} 
+        and your akhan name is ${femaleNames[dayNumber]}`
+    
+    } 
+    document.getElementById('result').innerHTML = result
+}
+
